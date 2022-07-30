afeb48321d3f91b844e72592724bcc3924bfe770
Bartosz Piech <piech.bartosz696@gmail.com>
2022-07-30 19:09:28 +0200

    added 205 working solution

--- 205/main.c
+++ 205/main.c
@@ -2,24 +2,26 @@
 #include <stdbool.h>
 #include <string.h>
 
-
-bool is_isomorphic(char * s, char * t){
-    char map[128] = { 0 }; // ascii table size
-    for (int i = 0; i < strlen(s); i++) {
-        if (map[t[i]] == 0 && map[s[i]] == 0) {
-            map[t[i]] = s[i];
-            map[s[i]] = t[i];
-        } else if (s[i] != map[t[i]]) {
+bool inner(char *first, char *second) {
+    char map[127] = { 0 };
+    for (int i = 0; i < strlen(first); i++) {
+        if (map[first[i]] == 0) {
+            map[first[i]] = second[i];
+        } else if (map[first[i]] != second[i]) {
             return false;
         }
     }
     return true;
 }
 
+bool isIsomorphic(char *s, char *t){
+    return inner(s, t) && inner(t, s);
+}
+
 
 int main() {
-    printf("true - %s\n", is_isomorphic("add", "egg") ? "true" : "false");
     printf("false - %s\n", is_isomorphic("foo", "bar") ? "true" : "false");
+    printf("true - %s\n", is_isomorphic("add", "egg") ? "true" : "false");
     printf("true - %s\n", is_isomorphic("paper", "title") ? "true" : "false");
     printf("false - %s\n", is_isomorphic("badc", "baba") ? "true" : "false");
     printf("true - %s\n", is_isomorphic("ACAB", "XCXY") ? "true" : "false");
