846815d5e75978894f6956ae0bad5b61c9f10ebe
Bartosz Piech <piech.bartosz696@gmail.com>
2022-07-31 17:31:03 +0200

    working solution for 21 using linked list splicing, there's room for improvement, but works for me

--- 21/main.c
+++ 21/main.c
@@ -5,16 +5,29 @@
  * Definition for singly-linked list.
  */
 
- typedef struct ListNode {
+ struct ListNode {
      int val;
      struct ListNode *next;
- } ListNode;
+ };
 
-void append(ListNode **h, int value) {
+typedef struct ListNode ListNode;
+
+void prepend(ListNode **h, int value) {
     ListNode *new = malloc(sizeof(ListNode));
     new->val = value;
     new->next = *h;
     *h = new;
+}
+
+void append(ListNode **h, int value) {
+    ListNode *new = malloc(sizeof(ListNode));
+    new->val = value;
+    if (*h == NULL) {
+        *h = new;
+        return;
+    }
+    new->next = (*h)->next;
+    (*h)->next = new;
 }
 
 void print(ListNode *h) {
@@ -26,43 +39,65 @@ void print(ListNode *h) {
     }
     printf("NULL }\n");
 }
-
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    ListNode *h = NULL;
-    while (list1 != NULL && list2 != NULL) {
-        if (list1->val < list2->val) {
-            append(&h, list1->val);
-            list1 = list1->next;
-        } else {
-            append(&h, list2->val);
-            list2 = list2->next;
+    /*
+    printf("!!\n");
+    print(list1);
+    print(list2);
+    */
+    if (list2 == NULL) {
+        return list1;
+    }
+    if (list1 == NULL) {
+        return list2;
+    }
+    ListNode *put = list2;
+    list2 = list2->next;
+
+    ListNode *tmp = list1;
+    ListNode *prev = NULL;
+    while (tmp != NULL) {
+        if (put->val <= tmp->val) {
+            //printf("%d %d\n", put->val, tmp->val);
+            put->next = tmp;
+            if (prev != NULL) {
+                prev->next = put;
+            } else {
+                list1 = put;
+            }
+            break;
         }
+        if (tmp->next == NULL) {
+            put->next = NULL;
+            tmp->next = put;
+            break;
+        }
+        prev = tmp;
+        tmp = tmp->next;
     }
-    while (list1 != NULL) {
-        append(&h, list1->val);
-        list1 = list1->next;
-    }
-    while (list2 != NULL) {
-        append(&h, list2->val);
-        list2 = list2->next;
-    }
-    return h;
+    /*
+    printf("@@\n");
+    print(list1);
+    print(list2);
+    */
+    return mergeTwoLists(list1, list2);
 }
 
 int main() {
     ListNode* h1 = NULL;
-    append(&h1, 4);
-    append(&h1, 2);
-    append(&h1, 1);
+    prepend(&h1, 4);
+    prepend(&h1, 2);
+    prepend(&h1, 1);
 
     ListNode* h2 = NULL;
-    append(&h2, 5);
-    append(&h2, 3);
-    append(&h2, 0);
+    prepend(&h2, 5);
+    prepend(&h2, 3);
+    prepend(&h2, 0);
 
     print(h1);
     print(h2);
 
     print(mergeTwoLists(h1, h2));
+    print(mergeTwoLists(NULL, h2));
     return 0;
 }
