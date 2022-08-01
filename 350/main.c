9ecb25a0f0e52fbe9a8f5880a94144cb6c65a48f
Bartosz Piech <piech.bartosz696@gmail.com>
2022-08-01 12:02:25 +0200

    solved 350 using primal 'hashmap' and it's 90% faster than everyone else's code, nice, adding notes too

--- 350/main.c
+++ 350/main.c
@@ -2,47 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+#define HASHMAPSIZE 1001
+
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
-    int *first = NULL;
-    int *last = NULL;
-    /*
-    int *longer = nums1Size >= nums2Size ? nums1 : nums2;
-    int longerSize = nums1Size >= nums2Size ? nums1Size : nums2Size;
-    for (int i = 0; i < longerSize; i++) {
-        for (int j = 0; j < 
-        if (
-    }
-    */
+    int numshm[HASHMAPSIZE][2] = { 0 };
+    *returnSize = 0;
     for (int i = 0; i < nums1Size; i++) {
-        for (int j = 0; j < nums2Size; j++) {
-            if (nums1[i] == nums2[j]) {
-                first = &nums1[i];
-                last = &nums1[i];
-                int ind1 = i;
-                int ind2 = j;
-                while (nums1[ind1++] == nums2[ind2++]) {
-                    last++;
-                }
-            }
+        numshm[nums1[i]][0]++;
+    }
+    for (int i = 0; i < nums2Size; i++) {
+        if (numshm[nums2[i]][1] < numshm[nums2[i]][0]) {
+            numshm[nums2[i]][1]++;
+            (*returnSize)++;
         }
     }
-    *returnSize = last - first;
     int *result = malloc(sizeof(int) * *returnSize);
-    memcpy(result, first, *returnSize * sizeof(int));
-    printf("first = %d\n", *first);
-    printf("last = %d\n", *last);
-    printf("len = %d\n", *returnSize);
+    int resultInd = 0;
+
+    int* shorter = nums1Size < nums2Size ? nums1 : nums2;
+    int shorterSize = nums1Size < nums2Size ? nums1Size : nums2Size;
+    for (int i = 0; i < shorterSize; i++) {
+        while (numshm[shorter[i]][1]-- > 0) {
+            result[resultInd++] = shorter[i];
+        }
+    }
     return result;
 }
 
 int main() {
-    int arr1[] = { 1,2,2,1 };
-    int arr2[] = { 2,2 };
+    int arr1[] = { 1000,1000,1000 };
+    int arr2[] = { 1000,1000 };
     int *resSize = malloc(sizeof(int));
-    int * res = intersect(arr1, 4, arr2, 2, resSize);
+    int * res = intersect(arr1, 3, arr2, 2, resSize);
     for (int i = 0; i < *resSize; i++) {
         printf("%d ", res[i]);
     }
