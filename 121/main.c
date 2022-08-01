41d4ebc3d28f9f7b10d3d84fa9ed82eeac3395ad
Bartosz Piech <piech.bartosz696@gmail.com>
2022-08-01 10:43:49 +0200

    still fighting with 121 profit (not really working)

--- 121/main.c
+++ 121/main.c
@@ -3,19 +3,25 @@
 #include <string.h>
 
 int maxProfit(int *prices, int pricesSize) {
-    int maxPrice = 0;
+    int minPrice = prices[0];
+    int minPriceInd = 0;
     for (int i = 0; i < pricesSize; i++) {
-        for (int j = i; j < pricesSize; j++) {
-            if (prices[j] - prices[i] > maxPrice) {
-                maxPrice = prices[j] - prices[i];
-            }
+        if (minPrice > prices[i]) {
+            minPrice = prices[i];
+            minPriceInd = i;
         }
     }
-    return maxPrice;
+    int maxPrice = minPrice;
+    for (int i = minPriceInd; i < pricesSize; i++) {
+        if (maxPrice < prices[i]) {
+            maxPrice = prices[i];
+        }
+    }
+    return maxPrice - minPrice;
 }
 
 int main() {
-    int arr1[] = { 7,6,4,3,1 };
-    printf("%d\n", maxProfit(arr1, 5));
+    int arr1[] = { 7,1,5,3,6,4 };
+    printf("%d\n", maxProfit(arr1, 6));
     return 0;
 }
