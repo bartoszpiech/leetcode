de1940b6cef42a3242d02eda62acd5a6ee63d363
Bartosz Piech <piech.bartosz696@gmail.com>
2022-08-01 10:44:34 +0200

    working 876 solution (but not with slow and fast pointers :/)

---
+++ 876/main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Definition for singly-linked list.
+ */
+
+ struct ListNode {
+     int val;
+     struct ListNode *next;
+ };
+
+typedef struct ListNode ListNode;
+
+void prepend(ListNode **h, int value) {
+    ListNode *new = malloc(sizeof(ListNode));
+    new->val = value;
+    new->next = *h;
+    *h = new;
+}
+
+void append(ListNode **h, int value) {
+    ListNode *new = malloc(sizeof(ListNode));
+    new->val = value;
+    if (*h == NULL) {
+        *h = new;
+        return;
+    }
+    new->next = (*h)->next;
+    (*h)->next = new;
+}
+
+void print(ListNode *h) {
+    ListNode *tmp = h;
+    printf("{ ");
+    while (tmp != NULL) {
+        printf("%d -> ", tmp->val);
+        tmp = tmp->next;
+    }
+    printf("NULL }\n");
+}
+
+struct ListNode* middleNode(struct ListNode* head){
+    size_t len = 0;
+    ListNode *tmp = head;
+    while (tmp) {
+        tmp = tmp->next;
+        len++;
+    }
+    tmp = head;
+    for (int i = 0; i < len / 2; i++) {
+        tmp = tmp->next;
+    }
+    return tmp;
+}
+
+int main() {
+    ListNode* h1 = NULL;
+    ListNode* h2 = NULL;
+    prepend(&h1, 5);
+    prepend(&h1, 4);
+    prepend(&h1, 3);
+    prepend(&h1, 2);
+    prepend(&h1, 1);
+
+    prepend(&h2, 6);
+    prepend(&h2, 5);
+    prepend(&h2, 4);
+    prepend(&h2, 3);
+    prepend(&h2, 2);
+    prepend(&h2, 1);
+    //print(h1);
+    print(middleNode(h1));
+    print(middleNode(h2));
+    return 0;
+}
