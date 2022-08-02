13edb57a7c775ce8ab2136e438520935f800e57b
Bartosz Piech <piech.bartosz696@gmail.com>
2022-08-02 13:40:59 +0200

    solved 566 with a little bit of help (missplaced c and r)

--- 566/main.c
+++ 566/main.c
@@ -3,35 +3,34 @@
 
 int** matrixReshape(int** mat, int matSize, int* matColSize, int r, int c, int* returnSize, int** returnColumnSizes) {
     if (r * c != matSize * *matColSize) {
+        *returnSize = matSize;
+        *returnColumnSizes = matColSize;
         return mat;
     }
     int **resultMatrix = malloc(sizeof(int *) * r);
-    *returnColumnSizes = malloc(sizeof(int));
-    *returnSize = r;
-    **returnColumnSizes = c;
-    //printf("%d %dsss\n", *returnSize, **returnColumnSizes);
+    int *retColSize = malloc(sizeof(int) * r);
     for (int i = 0; i < r; i++) {
         resultMatrix[i] = malloc(sizeof(int) * c);
+        retColSize[i] = c;
     }
     for (int i = 0; i < matSize; i++) {
-        for (int j = 0; j < *matColSize; j++) {
-            int ind = i * matSize + j;
-            //printf("ind: %d - ", ind);
-
-            int indc = ind / r;
-            int indr = ind - indc;
-            //printf("%d %d\n", indr, indc);
+        for (int j = 0; j < matColSize[i]; j++) {
+            int ind = i * matColSize[i] + j;
+            int indr = ind / c;
+            int indc = ind % c;
+            printf("$$%d - %d %d$$\n", ind, indr, indc);
             resultMatrix[indr][indc] = mat[i][j];
         }
     }
+    *returnSize = r;
+    *returnColumnSizes = retColSize;
     return resultMatrix;
-
 }
 
 void print(int **mat, int matSize, int* matColSize) {
     printf("{\n");
     for (int i = 0; i < matSize; i++) {
-        for (int j = 0; j < *matColSize; j++) {
+        for (int j = 0; j < matColSize[i]; j++) {
             printf("%d ", mat[i][j]);
         }
         printf("\n");
@@ -40,23 +39,22 @@ void print(int **mat, int matSize, int* matColSize) {
 }
 
 int main() {
-
     int rows = 2;
-    int cols = 2;
-    int *colsp = &cols;
+    int cols[] = { 2,2 };
     int **matrix = malloc(sizeof(int*) * rows);
     for (int i = 0; i < rows; i++) {
-        matrix[i] = malloc(sizeof(int) * cols);
+        matrix[i] = malloc(sizeof(int) * cols[i]);
     }
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+        for (int j = 0; j < cols[i]; j++) {
             matrix[i][j] = i * rows + j + 1;
         }
     }
-    print(matrix, rows, colsp);
+    print(matrix, rows, cols);
     int retsize;
     int *retcolsize;
-    int **matrix_converted = matrixReshape(matrix, rows, colsp, 1,4, &retsize, &retcolsize);
+    int **matrix_converted = matrixReshape(matrix, rows, cols, 4, 1, &retsize, &retcolsize);
+    printf("## %d %d ##\n", retsize, retcolsize[0]);
     print(matrix_converted, retsize, retcolsize);
     return 0;
 }
