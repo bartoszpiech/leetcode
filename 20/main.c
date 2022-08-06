a9a3a81f5c2250efec09bae0fd26ca15e2ddae9d
Bartosz Piech <piech.bartosz696@gmail.com>
2022-08-07 01:33:42 +0200

    added 20 char stack, but it's gotta be int stack so i'll modify it

---
+++ 20/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+    char val;
+    struct node_t *next;
+} node_t;
+
+typedef struct {
+    node_t *top;
+    size_t size;
+} stack_t;
+
+stack_t *stack_init() {
+    stack_t *new_stack = malloc(sizeof(stack_t));
+    new_stack->top = NULL;
+    new_stack->size = 0;
+    return new_stack;
+}
+
+bool stack_is_empty(stack_t *s) {
+    if (s->size <= 0) {
+        return true;
+    }
+    return false;
+}
+
+char stack_pop(stack_t *s) {
+    if (stack_is_empty(s)) {
+        fprintf(stderr, "Error, stack underflow\n");
+        exit(1);
+    }
+    char return_val = s->top->val;
+    node_t *tmp = s->top;
+    s->top = s->top->next;
+    free(tmp);
+    s->size--;
+    return return_val;
+}
+
+
+void stack_free(stack_t *s) {
+    while (!stack_is_empty(s)) {
+        stack_pop(s);
+    }
+    free(s);
+}
+
+void stack_print(stack_t *s) {
+    node_t *tmp = s->top;
+    while (tmp) {
+        printf("%c ->", tmp->val);
+        tmp = tmp->next;
+    }
+    printf(" NULL\n");
+}
+
+void stack_push(stack_t *s, char value) {
+    node_t *tmp = malloc(sizeof(node_t));
+    tmp->val = value;
+    tmp->next = s->top;
+    s->top = tmp;
+    s->size++;
+}
+
+bool isValid(char * s){
+    stack_t *stack = stack_init();
+    for (int i = 0; s[i] != '\0'; i++) {
+        stack_push(stack, s[i])
+    }
+    stack_free(stack);
+
+}
+
+int main() {
+    printf("%s\n", isValid("()") ? "true" : "false");
+    printf("%s\n", isValid("()[]{}") ? "true" : "false");
+    printf("%s\n", isValid("(]") ? "true" : "false");
+    return 0;
+}
--- 74/main.c
+++ 74/main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+bool searchMatrix(int matrix[][4], int matrixSize, int* matrixColSize, int target){
+    // search row
+    int lind = 0, rind = matrixSize;
+    int ind;
+    if (target > matrix[matrixSize - 1][matrixColSize[matrixSize - 1] - 1] ||
+        target < matrix[0][0]) {
+        return false;
+    }
+    while (lind <= rind) {
+        int mid = lind + (rind - lind) / 2;
+        printf("%d - %d %d\n", mid, lind, rind);
+        printf("%d %d ---\n", matrix[mid][0], matrix[mid][matrixColSize[mid] - 1]);
+        if (matrix[mid][0] <= target && matrix[mid][matrixColSize[mid] - 1] >= target) {
+            ind = mid;
+            break;
+        }
+        else if (matrix[mid][0] > target) {
+            rind = mid;
+        }
+        else if (matrix[mid][0] < target) {
+            lind = mid;
+        } else {
+            ind = -1;
+            break;
+        }
+    }
+    // search inside the row
+    printf("%d -- row", ind);
+}
 
-
-void print(int **mat, int matSize, int* matColSize) {
+void print(int mat[][4], int matSize, int* matColSize) {
     printf("{\n");
     for (int i = 0; i < matSize; i++) {
         for (int j = 0; j < matColSize[i]; j++) {
@@ -15,11 +44,15 @@ void print(int **mat, int matSize, int* matColSize) {
 }
 
 int main() {
-    int rows = 6;
-    int retsize;
-    int *retcolsize;
-    int **triangle = generate(rows, &retsize, &retcolsize);
-    //printf("## %d %d ##\n", retsize, retcolsize[0]);
-    print(triangle, retsize, retcolsize);
+    int matrix[3][4] = {
+        {1,3,5,7},
+        {10,11,16,20},
+        {23,30,34,60}
+    };
+    int matrixSize = 3;
+    int matrixColSize[] = { 4, 4, 4, 4 };
+    int target = 8;
+    print(matrix, matrixSize, matrixColSize);
+    searchMatrix(matrix, matrixSize, matrixColSize, target);
     return 0;
 }
