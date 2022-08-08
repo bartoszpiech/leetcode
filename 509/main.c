5a12d556bc9facd71a12d79ddcf69f992dcc1d15
Bartosz Piech <piech.bartosz696@gmail.com>
2022-08-08 12:09:39 +0200

    added 509 fibbonacci sequence, got recursive method with dynamic programming memory of previous numbers, i got improvement from faster than 15% -> 55%

---
+++ 509/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define MEMSIZE 100
+int fib(int n){
+    static int mem[MEMSIZE];
+    static int flag = 0;
+    if (flag == 0) {    // fill the memory
+        mem[0] = 0;
+        mem[1] = 1;
+        for (int i = 2; i < MEMSIZE; i++) {
+            mem[i] = -1;
+        }
+        flag = 1;
+    }
+    if (mem[n] == -1) {
+        mem[n] = fib(n - 1) + fib(n - 2);
+    }
+    return mem[n];
+}
+
+int main() {
+    printf("%d\n", fib(2));
+    printf("%d\n", fib(5));
+    printf("%d\n", fib(40));
+    return 0;
+}
